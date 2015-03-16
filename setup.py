283b1d51979fb52f68b7b9a1eb8294e8f6363a81
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2015-03-16 16:57:40 +0100

    ignore configuration files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='learning-python',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='signed',
+    author_email='',
+    description=''
+)
