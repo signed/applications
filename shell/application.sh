9f8cd04b665e1ffde8bb2875e650292c1f83c9f6
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2015-04-04 00:38:20 +0200

    Use export to expose environment variables

--- shell/application.sh
+++ shell/application.sh
@@ -1,7 +1,3 @@
-source_file(){
-    . $1
-}
-
 add_to_path(){
     if [ ! -d "$1" ]; then
         echo "not a directory "$1
@@ -9,17 +5,20 @@ add_to_path(){
     fi
 
     if [ ":$STANDIN:" != *":$1:"* ]; then
-        STANDIN="${STANDIN:+"$STANDIN:"}$1"
+        STANDIN="$1:${STANDIN:+"$STANDIN"}"
     fi
 }
 
-# load all declared environment variables
+# export all declared environment variables
 for file in $(find etc/*.env -type f)
 do
-    source_file ${file}
+    for path_entry in $(sed -E '$s/(.*\S+.*)/\1\n/' $file)
+    do
+        export ${path_entry}
+    done
 done
 
-# append all declared paths to the PATH
+# prepend all declared paths to the PATH
 for file in $(find etc/*.path -type f)
 do
     for path_entry in $(sed -E '$s/(.*\S+.*)/\1\n/' $file)
