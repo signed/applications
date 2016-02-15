49a0a67108ed4dc79f8884f4e5c74fd5a47a0021
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2016-02-15 20:46:34 +0100

    Extracted DirectoryStructure class

--- src/applications.py
+++ src/applications.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
-import sys
-
-import hashlib
-import os
-import requests
-import shutil
-import tarfile
 import errno
+import hashlib
+import shutil
+import sys
+import tarfile
 import urlparse
 import zipfile
+
+import os
+import requests
 from os.path import expanduser
 from os.path import join
 
@@ -51,32 +51,58 @@ class _MyHackedTarFile(tarfile.TarFile):
                 self._dbg(1, "tarfile: %s" % e)
 
 
+class DirectoryStructure:
+    def __init__(self, path):
+        self.path = path
+        self.configuration_path = os.path.join(self.path, "etc")
+
+    def ensure_directories_are_setup(self):
+        mkdir_p(self.path)
+        mkdir_p(self.configuration_path)
+
+    def ensure_installation_directory_exists(self, application):
+        mkdir_p(self._parent_directory_for(application))
+
+    def current_symlink_path_for(self, application):
+        return join(self._parent_directory_for(application), 'current')
+
+    def archive_path_for(self, application):
+        return join(self._parent_directory_for(application), application.filename())
+
+    def directory_for(self, application):
+        return os.path.join(self._parent_directory_for(application), application.version)
+
+    def archive_already_extracted(self, application):
+        return os.path.isdir(self.directory_for(application))
+
+    def _parent_directory_for(self, application):
+        return os.path.join(self.path, application.name)
+
+
 class ApplicationsHome:
     def __init__(self, path, downloader):
-        self.path = expanduser(path)
-        self.configuration_path = os.path.join(self.path, "etc")
+        self.directory_structure = DirectoryStructure(expanduser(path))
         self.downloader = downloader
 
     def ensure_environment_is_setup(self):
-        mkdir_p(self.path)
-        mkdir_p(self.configuration_path)
+        self.directory_structure.ensure_directories_are_setup()
         self._write_rc_file()
 
     def install(self, application):
-        self._ensure_installation_directory_exists(application)
+        self.directory_structure.ensure_installation_directory_exists(application)
         self._ensure_archive_was_downloaded(application)
         self._extract_archive(application)
         self._ensure_current_symlink_is_up_to_date(application)
 
         path = application.metadata_for('path')
         if path is not None:
-            path_to_path_file = os.path.join(self.configuration_path, application.name + '.path')
+            path_to_path_file = os.path.join(self.directory_structure.configuration_path, application.name + '.path')
             with open(path_to_path_file, 'wt') as path_file:
                 path_file.write(path % self._template_data_for(application))
 
         env = application.metadata_for('env')
         if env is not None:
-            path_to_env_file = os.path.join(self.configuration_path, application.name + '.env')
+            path_to_env_file = os.path.join(self.directory_structure.configuration_path, application.name + '.env')
             with open(path_to_env_file, 'wt') as env_file:
                 template_content = '\n'.join(map(lambda key, value: key + '="' + value + '"', env.items()))
                 env_file.write(template_content % self._template_data_for(application))
@@ -85,56 +111,41 @@ class ApplicationsHome:
         path_to_rc_script = _search_path_for('shell/application.sh')
         with open(path_to_rc_script, 'r') as rc_file:
             rc_file_template = rc_file.read()
-        path_to_destination = os.path.join(self.path, 'application.rc')
+        path_to_destination = os.path.join(self.directory_structure.path, 'application.rc')
         with open(path_to_destination, 'w')as rc_file_installed:
-            replacement = "application_directory='%(path)s'" % {'path': self.path}
+            replacement = "application_directory='%(path)s'" % {'path': self.directory_structure.path}
             rc_file_installed.write(rc_file_template.replace("application_directory='/tmp'", replacement))
 
-    def _template_data_for(self, application):
-        return {'installation_directory': self._current_symlink_path_for(application)}
-
-    def _ensure_installation_directory_exists(self, application):
-        mkdir_p(self._parent_directory_for(application))
-
     def _ensure_archive_was_downloaded(self, application):
-        self.downloader.download(application, self._archive_path_for(application))
+        self.downloader.download(application, self.directory_structure.archive_path_for(application))
+
+    def _template_data_for(self, application):
+        return {'installation_directory': self.directory_structure.current_symlink_path_for(application)}
 
     def _extract_archive(self, application):
-        if self._archive_already_extracted(application):
+        if self.directory_structure.archive_already_extracted(application):
             print('already extracted ' + application.filename())
             return
 
-        archive_path = self._archive_path_for(application)
-        target_directory_path = self._directory_for(application)
+        archive_path = self.directory_structure.archive_path_for(application)
+        target_directory_path = self.directory_structure.directory_for(application)
 
         ArchiveExtractor().extract(archive_path, target_directory_path)
 
     def _ensure_current_symlink_is_up_to_date(self, application):
-        current_sym_link = self._current_symlink_path_for(application)
+        current_sym_link = self.directory_structure.current_symlink_path_for(application)
 
         if os.path.islink(current_sym_link):
             os.unlink(current_sym_link)
 
-        extract_directory = self._directory_for(application)
+        extract_directory = self.directory_structure.directory_for(application)
         os.symlink(extract_directory + '/', current_sym_link)
-
-    def _current_symlink_path_for(self, application):
-        return join(self._parent_directory_for(application), 'current')
-
-    def _archive_path_for(self, application):
-        return join(self._parent_directory_for(application), application.filename())
-
-    def _parent_directory_for(self, application):
-        return os.path.join(self.path, application.name)
-
-    def _directory_for(self, application):
-        return os.path.join(self._parent_directory_for(application), application.version)
-
-    def _archive_already_extracted(self, application):
-        return os.path.isdir(self._directory_for(application))
 
 
 class Downloader:
+    def __init__(self):
+        pass
+
     def download(self, application, destination):
         if os.path.isfile(destination):
             print('already downloaded ' + application.filename())
@@ -269,6 +280,6 @@ if __name__ == '__main__':
     applications.ensure_environment_is_setup()
 
     applications.install(oracle_java())
-    applications.install(maven())
-    applications.install(intellij())
-    applications.install(xmind())
+    # applications.install(maven())
+    # applications.install(intellij())
+    # applications.install(xmind())
