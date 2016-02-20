b853c387ea45e941761a8e6fa8b1314d82adbdc1
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2016-02-20 18:32:32 +0100

    improve tests

--- test/applications/test_writeenvironmentvariablefile.py
+++ test/applications/test_writeenvironmentvariablefile.py
@@ -18,18 +18,20 @@ class WriteEnvironmentVariableFileTest(TestCase):
 
     @mock.patch.object(FileWriter, 'write_to')
     def test_do_nothing_if_there_are_no_environment_variables_set(self, mock_write_to):
-        step = WriteEnvironmentVariableFile()
-        step.install(None, self._application(), None)
+        self.template_data ={'no_env': "some value"}
+        self.execute_step()
         self.assertFalse(mock_write_to.called, "Do not try to write the env file if there are no env variables")
 
     @mock.patch.object(FileWriter, 'write_to')
     def test_write_existing_environment_variables_into_the_env_file(self, mock_write_to):
-        step = WriteEnvironmentVariableFile()
         self.metadata['env'] = {
             'NAME': 'VALUE'
         }
-        step.install(self.structure, self._application(), self.template_data)
+        self.execute_step()
         mock_write_to.assert_called_with('/tmp/etc/do_not_care.env', 'wt', 'NAME="VALUE"')
+
+    def execute_step(self):
+        WriteEnvironmentVariableFile().install(self.structure, self._application(), self.template_data)
 
     def _application(self):
         return Application(self.name, self.version, self.url, self.metadata)
