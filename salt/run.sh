38a25ac4ff98aba8707e44205acb3144c6be9c93
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2016-02-27 15:18:04 +0100

    fix paths

--- salt/run.sh
+++ salt/run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
-sudo salt-call --local --config-dir=./salt/configuration --file-root=./salt/root --pillar-root=./salt/pillar state.highstate
+sudo salt-call --local --config-dir=./configuration --file-root=./root --pillar-root=./pillar state.highstate
 
